80d6e097e9a09053d05b2ad8930a7ef367592805
Asbjørn Skødt <asbjoern.skoedt@gmail.com>
2023-07-04 16:54:44 +0200

    commit

--- src/software/archiveIndex/vis-archiveIndex.php
+++ src/software/archiveIndex/vis-archiveIndex.php
@@ -10,8 +10,6 @@
           <div class="arc-content">
             <h2>archiveIndex</h2>
             <h3>Vedrørende din brug af servicen</h3>
-			        <p>I udgangspunktet er metadata om en arkiveringsversions proveniens eller stamdata ikke følsomme eller hemmelige informationer. Håndterer du klassificerede data, kan der gælde andre forhold.</p>
-			        <p>Oplysninger indtastet ved brug af denne service, registreres ingen steder af Arkivkonsulent eller andre parter. Data indlæses i serverens hukommelse og skrives til og fra XML-filer, der opbevares på brugerens egen computer eller serveren. Når forbindelsen brydes af bruger eller server, forsvinder data fra serverens hukommelse med det samme og fil slettes i fillageret efter 15 min. Forbindelsen mellem bruger og server er krypteret. Arkivkonsulents webhotelleverandør vurderes at være en troværdig og sikker databehandler. Har du generelt ikke tillid til brugen af internettet, skal du ikke bruge denne service.</p>
           </div>
         </div>
 		<?php include('../../templates/footer.php') ?>
